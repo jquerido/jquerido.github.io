f34623557b1613223197ca5c9916d4c5d33c0fcb
Jessica Querido <jessica.querido@dcmail.ca>
2018-02-14 08:27:31 -0500

    upload header and footer

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+
+<?php
+echo "<!--
+		Name: 		 Jessica Querido
+		File: 		 $filename
+		Date: 		 $date
+		Description: $description
+	-->";?>
+	
+	
+	<?php echo "<title>$title</title>"; ?>
+	<link rel="stylesheet" type="text/css" href="./style.css"/>
+	
+	</head>
+	
+	<body>
+
+<div id="container">
+	<div id="header">
+		<img src="./images/logo.png" alt="Durham College" />
+		<?php echo "<h1>$banner</h1>"?>
+	</div>
+	<div id="content-container">
+		<div id="navigation">
+			<h3>
+				Local Navigation Bar
+			</h3>
+			<ul>
+				<li><a href="./index.php">WEBD2201 Home Page</a></li>
+				<li><a href="./lab1.php">Lab 2</a></li>
+			</ul>
+		</div>
+		<div id="content">
+		
